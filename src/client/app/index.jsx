990f40cffd8d142982535832e2dab84f590a29b0
Qasim Laghari <go.jets.laghari@gmail.com>
2016-06-03 19:16:51 -0500

    Add App component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,12 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
+import App from './components/app';
 
-class App extends React.Component {
-  render () {
-    return (
-    <p>Let's build the Parq Web Client</p>
-    );
-  }
-}
-
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));

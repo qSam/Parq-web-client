5c152c2138d4745cad0a349f6d5b7473b69a0589
Qasim Laghari <go.jets.laghari@gmail.com>
2016-06-06 12:39:13 -0500

    Add Signin component

---
+++ src/client/app/components/auth/signin.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import * as actions from '../../actions';
+
+class Signin extends Component {
+
+  render(){
+    cosnt {handleSubmit, fields: {email,password}} = this.props;
+
+    return(
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))} >
+
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input {...email} className="form-control" />
+        </fieldset>
+
+        <fieldset className="form-group">
+                  <label>Password:</label>
+                  <input {...password} className="form-control" />
+        </fieldset>
+
+        <button action="submit" className="btn btn-primary">Sign In</button>
+
+
+      </form>
+    );
+  }
+}
+
+
+
+
+
+
+
+export default reduxForm({
+    form: 'signin',
+    fields: ['email','password']
+}, mapStateToProps, actions) (Signin);
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -5,9 +5,11 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
 import Welcome from './components/welcome';
+import Signin from './components/auth/signin';
 
 render(<Router history={browserHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={Welcome} />
+          <Route path="signin" component={Signin} />
         </Route>
        </Router>, document.getElementById('app'));

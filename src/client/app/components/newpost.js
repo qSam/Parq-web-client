f2633d2ca646d54fcf941672652ef9f87ac483fb
Qasim Laghari <go.jets.laghari@gmail.com>
2016-06-28 21:26:32 -0500

    Add new post component

---
+++ src/client/app/components/newpost.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+
+class NewPost extends Component {
+
+  render() {
+    return (
+      <div>Add a new post</div>
+    );
+  }
+}
